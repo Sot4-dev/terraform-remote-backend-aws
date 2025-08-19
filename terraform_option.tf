189efbbeedc052ab51de6ac8612bb4681ee9f854
Sot4 <Sot4.develop@gmail.com>
2025-08-19 17:10:12 +0900

    バックエンド設定をkey以外backend.hclから読み込むように変更。backend.hclを.gitignoreに追記

--- terraform_option.tf
+++ terraform_option.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "s3" {
-    bucket = "sot4-learning-bucket-1"
     key = "global/s3/terraform.tfstate"
-    region = "ap-northeast-1"
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt = true
   }
 }
